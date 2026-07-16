c60e49eee9559811fc7fc4973ba8005ce117261d
KahnishgaSolaidurairaj <krishnakani19@gmail.com>
2026-07-15 23:23:09 -0400

    new xcode project

---
+++ mindMatters/mindMattersApp.swift
@@ -0,0 +1,17 @@
+//
+//  mindMattersApp.swift
+//  mindMatters
+//
+//  Created by Kahnishga Solaidurairaj on 7/15/26.
+//
+
+import SwiftUI
+
+@main
+struct mindMattersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
